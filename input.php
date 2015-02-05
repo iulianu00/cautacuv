479e30f1297d4a2ea7c3a5cad5e0f04c4208adf1
iulianu00 <iuliandoran@gmail.com>
2015-02-05 17:48:42 +0200

    Create input.php

---
+++ input.php
@@ -0,0 +1,16 @@
+<?php
+
+echo ' 
+
+<form action="index.php">
+Introdu te rog textul in care doresti sa cauti un cuvant:<br>
+<input type="text" name="text_introdus" size="60" height="1" >
+<br>
+Introdu te rog cuvantul cautat:<br>
+<input type="text" name="cuvant_cautat" size="40">
+<br><br>
+<input type="submit" value="Submit">
+</form>
+	   
+	   ';
+	   
